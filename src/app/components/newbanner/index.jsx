64b2c6b9638553a330e5309d95231bc3efdf1dcd
Ceyhun Isgandarov <ceyhunisgandarov@MacBook-Air.local>
2025-10-16 13:45:16 +0400

    update first carousel slide

--- src/app/components/newbanner/index.jsx
+++ src/app/components/newbanner/index.jsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import styles from "../../../../public/assets/css/module/newbanner/banner.module.css";
 
-
 export default function Banner({ carousel, t, active }) {
   console.log(carousel.id + "," + active);
   const imageUrl = carousel?.imageLink
@@ -16,17 +15,22 @@ export default function Banner({ carousel, t, active }) {
         backgroundImage: `url('${imageUrl}')`,
       }}
     >
-      <div className={styles.blurStripe}>
-        <h1 className={`${styles.title} ${active ? styles.active : ""}`}>
-          {carousel.title}
-        </h1>
-        <p className={`${styles.subtitle} ${active ? styles.active : ""}`}>
-          {carousel.description ?? ""}
-        </p>
-        <Link href={`/${t('locale')}/${carousel.link}`} className={`${styles.btnMore} ${active ? styles.active : ""}`}>
-          {t("more")}
-        </Link>
-      </div>
+      {carousel.id !== 1 && (
+        <div className={styles.blurStripe}>
+          <h1 className={`${styles.title} ${active ? styles.active : ""}`}>
+            {carousel.title}
+          </h1>
+          <p className={`${styles.subtitle} ${active ? styles.active : ""}`}>
+            {carousel.description ?? ""}
+          </p>
+          <Link
+            href={`/${t("locale")}/${carousel.link}`}
+            className={`${styles.btnMore} ${active ? styles.active : ""}`}
+          >
+            {t("more")}
+          </Link>
+        </div>
+      )}
       <div className={styles.overlay}></div>
     </section>
   );
